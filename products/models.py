f35615fc80fa80d36ef36f81e181321e0bb42fe2
Mohammad daneshmand <81333964+mohammadDV@users.noreply.github.com>
2023-04-16 16:51:33 +0300

    add product app

---
+++ products/models.py
@@ -0,0 +1,45 @@
+from django.db import models
+from django.utils.translation import ugettext_lazy as _
+
+
+
+class Category(models.Model):
+    parent = models.ForeignKey('self',verbose_name=_('parent'), blank=True, null=True, on_delete=models.CASCADE)
+    title = models.CharField(_('title'),max_length=200)
+    description = models.TextField(_('description'))
+    avatar = models.ImageField(_('avatar'),blank=True,upload_to='categories/')
+    is_enable = models.BooleanField(_('is_enable'), default=True)
+    created_at = models.DateTimeField(_('created time'),auto_now_add=True)
+    updated_at = models.DateTimeField(_('updated time'),auto_now=True)
+
+    class Meta:
+        db_table = 'categories'
+        verbose_name = _('category')
+        verbose_name_plural = _('categories')
+
+class Product(models.Model):
+    title = models.CharField(_('title'),max_length=200)
+    description = models.TextField(_('description'))
+    avatar = models.ImageField(_('avatar'),blank=True,upload_to='categories/')
+    categories = models.ManyToManyField('Category', verbose_name=_('categories'),blank=True)
+    is_enable = models.BooleanField(_('is_enable'), default=True)
+    created_at = models.DateTimeField(_('created time'),auto_now_add=True)
+    updated_at = models.DateTimeField(_('updated time'),auto_now=True)
+
+    class Meta:
+        db_table = 'products'
+        verbose_name = _('product')
+        verbose_name_plural = _('products')
+
+class File(models.Model):
+    product = models.ForeignKey('Product', verbose_name=_('product'), on_delete=models.CASCADE)
+    title = models.CharField(_('title'),max_length=200)
+    file = models.FileField(_('file'),upload_to='files/%Y/%m/%d/')
+    is_enable = models.BooleanField(_('is_enable'), default=True)
+    created_at = models.DateTimeField(_('created time'),auto_now_add=True)
+    updated_at = models.DateTimeField(_('updated time'),auto_now=True)
+
+    class Meta:
+        db_table = 'files'
+        verbose_name = _('file')
+        verbose_name_plural = _('files')
